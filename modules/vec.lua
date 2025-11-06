16dd6f9a6f393ac1f9dc05ab89a8f4aec4871ead
Caique Costa <ccostafrias@gmail.com>
2025-11-06 19:57:10 -0300

    creates two utility vec functions

--- modules/vec.lua
+++ modules/vec.lua
@@ -8,6 +8,17 @@ function normalize(v)
     v.y = v.y / mod
 end
 
+function distance(v1, v2)
+    return math.sqrt((v2.x - v1.x)^2 + (v2.y - v1.y)^2)
+end
+
+function midpoint(v1, v2)
+    return { 
+        x = (v1.x + v2.x) / 2, 
+        y = (v1.y + v2.y) / 2 
+    }
+end
+
 function nullVec(v)
     if v.x == 0 and v.y == 0 then
         return true

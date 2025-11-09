b26b0fee1d0ed8b3380be8328405f8cc5a1d2702
Caique Costa <ccostafrias@gmail.com>
2025-11-09 13:24:00 -0300

    fix offset in shaders
    Offset is now passed as param

--- modules/shaders.lua
+++ modules/shaders.lua
@@ -2,8 +2,8 @@ outlineShader = love.graphics.newShader("shaders/outline.glsl")
 outlineShader:send("thickness", 15.0)
 outlineShader:send("outlineColor", {1, 1, 1, 1})
 
-function drawSpriteWithOutline(image, x, y, scale)
+function drawSpriteWithOutline(image, x, y, scale, offset)
     love.graphics.setShader(outlineShader)
-    love.graphics.draw(image, x - image:getWidth()/2, y - image:getHeight()/2, 0, scale, scale)
+    love.graphics.draw(image, x, y, 0, scale, scale, offset.x, offset.y)
     love.graphics.setShader()
 end
